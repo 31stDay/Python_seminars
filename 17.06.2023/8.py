fe504ef9825ba8541c43d9b4c91b5504addd0807
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-06-25 18:14:25 +0300

    Task 8 has been done

--- 17.06.2023/8.py
+++ 17.06.2023/8.py
@@ -7,3 +7,10 @@
 3 2 1 -> no
 """
 
+m = int(input('Please enter width of the chocolate bar: '))
+n = int(input('Please enter length of the chocolate bar: '))
+k = int(input('Please enter the amount of slices: '))
+
+if (k < m * n) and ((k % m == 0) or (k % n == 0)):
+    print('Yes')
+else: print('No')
