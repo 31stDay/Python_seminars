cea3ff99d480bad4cdcda1716b8c3d7b33a6e0ca
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-06-25 16:49:06 +0300

    In progress

--- 17.06.2023/2.py
+++ 17.06.2023/2.py
@@ -1,8 +1,14 @@
 """
 Задача 2: Найдите сумму цифр трехзначного числа.
-
 *Пример:*
-
 123 -> 6 (1 + 2 + 3)
 100 -> 1 (1 + 0 + 0) |
 """
+
+number = int(input('Please enter three digit number:'))
+digits_list = []
+t_number = number
+
+if (number > 99) and (number < 1000):
+    digits_list.append(t_number % 10)
+    t_number = t_number // 10
