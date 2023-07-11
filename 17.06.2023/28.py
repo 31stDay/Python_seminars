7b745f70ea279a1df08c42adb014c78d643d6650
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-07-11 23:06:22 +0300

    Task 28 has been done

--- 17.06.2023/28.py
+++ 17.06.2023/28.py
@@ -6,3 +6,13 @@
 # 2 2
 #     4 
 
+def sum_rec(a, b):
+    if a == 0:
+        return b
+    else:
+        return sum_rec(a - 1, b + 1)
+    
+a = int(input('Enter your number: '))
+b = int(input('Please your second number: '))
+
+print(f'{a} plus {b} is equal {sum_rec(a, b)}')  
